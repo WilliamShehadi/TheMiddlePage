bcba59b7634fef908669846983a97496ff97c9b8
WilliamShehadi <ws2515@nyu.edu>
2024-10-22 12:33:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//JS (For scrolling effect)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
